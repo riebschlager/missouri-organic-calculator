87690900f332e8ce6e69ebae7dbfd1a4d5c0894b
Chris Riebschlager <riebschlager@gmail.com>
2015-05-20 21:07:24 -0500

    yeah buddy

--- src/app/index.js
+++ src/app/index.js
@@ -6,6 +6,15 @@ angular.module('missouriOrganicCalculator')
     .controller('MainCtrl', ['$scope', '$log',
         function($scope, $log) {
 
+            var form = document.getElementById('block-yui_3_17_2_3_1432168251676_5773');
+            form.style.display = 'none';
+
+            var copyBlock = document.getElementById('block-yui_3_17_2_9_1432168251676_6295');
+            copyBlock.style.display = 'none';
+
+            var fQuantity = document.getElementById('hidden-yui_3_17_2_1_1432168251676_51805');
+            var fProduct = document.getElementById('hidden-yui_3_17_2_1_1432168251676_51217');
+
             $log.debug('MainCtrl');
 
             $scope.bed = {};
@@ -17,6 +26,7 @@ angular.module('missouriOrganicCalculator')
 
             $scope.setProduct = function(product) {
                 $scope.product = product;
+                fProduct.value = product.name;
                 if (!$scope.bed.depth) {
                     $scope.bed.depth = $scope.product.recommendedDepth;
                 }
@@ -81,6 +91,9 @@ angular.module('missouriOrganicCalculator')
                     if ($scope.bed.width && $scope.bed.length && $scope.bed.depth && $scope.product.name) {
                         $scope.bed.totalCubicYards = (($scope.bed.width * $scope.bed.length * ($scope.bed.depth / 12)) / 27).toFixed(2);
                         $scope.bed.totalPrice = $scope.product.price * $scope.bed.totalCubicYards;
+                        fQuantity.value = $scope.bed.totalCubicYards + ' cubic yards';
+                        form.style.display = 'block';
+                        copyBlock.style.display = 'block';
                     } else {
                         $scope.bed.totalCubicYards = undefined;
                         $scope.bed.totalPrice = undefined;
@@ -92,6 +105,9 @@ angular.module('missouriOrganicCalculator')
                     if ($scope.bed.diameter && $scope.bed.depth && $scope.product.name) {
                         $scope.bed.totalCubicYards = ((Math.PI * Math.pow(($scope.bed.diameter / 2), 2) * ($scope.bed.depth / 12)) / 27).toFixed(2);
                         $scope.bed.totalPrice = $scope.product.price * $scope.bed.totalCubicYards;
+                        fQuantity.value = $scope.bed.totalCubicYards + ' cubic yards';
+                        form.style.display = 'block';
+                        copyBlock.style.display = 'block';
                     } else {
                         $scope.bed.totalCubicYards = undefined;
                         $scope.bed.totalPrice = undefined;
@@ -103,6 +119,9 @@ angular.module('missouriOrganicCalculator')
                     if ($scope.bed.diameter && $scope.bed.depth && $scope.product.name) {
                         $scope.bed.totalCubicYards = (((Math.PI * Math.pow(($scope.bed.diameter / 2), 2) * ($scope.bed.depth / 12)) / 27) / 2).toFixed(2);
                         $scope.bed.totalPrice = $scope.product.price * $scope.bed.totalCubicYards;
+                        fQuantity.value = $scope.bed.totalCubicYards + ' cubic yards';
+                        form.style.display = 'block';
+                        copyBlock.style.display = 'block';
                     } else {
                         $scope.bed.totalCubicYards = undefined;
                         $scope.bed.totalPrice = undefined;
